7b15ed37d1e1186f09f8a6ba6ef7a9b77b67144d
covermaster00 <79508914+covermaster00@users.noreply.github.com>
2024-06-24 13:54:42 +0300

    First alpha relise

---
+++ Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace GPAgent
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(128, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(40, 211);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(852, 339);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(40, 97);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(852, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "http://172.20.195.51:8080/videos/DCIM/100GOPRO/";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 150);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 556);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(914, 23);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(938, 583);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Reflection; // Логирование
+
+using System.Net.NetworkInformation; // Работа с сетевыми адаптерами
+using System.Management;    // Работа с событиями
+
+namespace GPAgent
+{
+    public partial class Form1 : Form
+    {
+        public bool Connecting = false;
+
+        const int WM_DEVICECHANGE = 0x219;
+        const int DBT_DEVICEARRIVAL = 0x8000;
+        const int DBT_DEVICEREMOVECOMPLETE = 0x8004;
+        const int DBT_DEVTYP_VOLUME = 0x00000002;
+
+        private BackgroundWorker backgroundWorker1;
+        // Структура для передачи параметров в субпоток
+        // https://professorweb.ru/my/WPF/documents_WPF/level31/31_3.php
+        public class DownloadParameters
+        {
+            public string GoPro
+            { get; set; }
+
+            public DownloadParameters(string gopro)
+            {
+                GoPro = gopro;
+            }
+
+        }
+
+        List<string> serials = new List<string>();
+
+    //    private DriveDetector driveDetector = null;
+
+        // Главная форма
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer1.Stop();
+
+            // Загрузка конфигурации из файла XML
+            System.Xml.Serialization.XmlSerializer reader = new System.Xml.Serialization.XmlSerializer(typeof(XMLConfig));
+            System.IO.StreamReader file = new System.IO.StreamReader(@"config.xml");
+            Global.Config = (XMLConfig)reader.Deserialize(file);
+            
+            if (Global.Config.MakeWorkLogs) Logger.WriteLine("Конфигурация XML загружена");
+
+            // Организация фоновой работы
+            // https://learn.microsoft.com/ru-ru/dotnet/desktop/winforms/controls/how-to-download-a-file-in-the-background?view=netframeworkdesktop-4.8 
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+
+
+        }
+
+        // Событие подключения GoPro
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);   
+
+            // Определяем подключение USB устройства
+            if ((m.Msg == WM_DEVICECHANGE) && !Connecting)
+            //MessageBox.Show("USB");
+            {
+                
+                timer1.Start();
+                
+                MessageBox.Show(m.WParam.ToString());
+            }
+         
+        }
+
+
+       
+        // Основной фоновый процесс - скачивание файлов
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            // Получить входные значения
+            DownloadParameters input = (DownloadParameters)e.Argument;
+
+   //         MessageBox.Show(input.GoPro);
+
+
+            // Получение страницы со списком файлов с камеры
+
+            WebRequest request = WebRequest.Create(input.GoPro);
+            request.Method = "GET";
+            WebResponse response = request.GetResponse();
+            if (Global.Config.MakeWorkLogs) Logger.WriteLine("Подключение камеры " + input.GoPro);
+
+            string answer = string.Empty;
+            using (Stream s = response.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(s))
+                {
+                    answer = reader.ReadToEnd();
+                }
+            }
+            response.Close();
+
+
+            // Парсинг html страницы, извлечение имен файлов
+            List<string> names = new List<string>();
+            for (var m = Regex.Match(answer, @"GX[0-9]{6}\.MP4"); m.Success; m = m.NextMatch())
+            {
+                names.Add(m.Value);
+//                MessageBox.Show(m.Value);
+            }
+            // удаление дубликатов
+            IEnumerable<string> distinctNames = names.AsQueryable().Distinct();
+
+            // Папка назначения
+            string destPath = Global.Config.DestPath;
+
+            WebClient client = new WebClient();
+            foreach (var name in distinctNames)
+            {
+                MessageBox.Show(input.GoPro + name);
+                client.DownloadFile(new Uri(input.GoPro + name), destPath + name);
+                if (Global.Config.MakeWorkLogs) Logger.WriteLine("Копирование файла " + input.GoPro + name);
+   //             backgroundWorker1.ReportProgress(100 / distinctNames.Count());
+                
+            }
+            
+        }
+
+        // Событие по завершении выполнения
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            // Set progress bar to 100% in case it's not already there.
+            progressBar1.Value = 100;
+
+            if (e.Error == null)
+            {
+                MessageBox.Show("Download Complete");
+            }
+            else
+            {
+                MessageBox.Show(
+                    "Failed to download file",
+                    "Download failed",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+            progressBar1.Value = 0;
+        }
+
+        // Событие изменения в фоновой функции
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            this.progressBar1.Value = e.ProgressPercentage;
+        }
+
+  
+
+        // ОСНОВНАЯ КНОПКА
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            // Блок модно удалить после тестов
+            WebRequest request = WebRequest.Create(textBox1.Text);
+            request.Method = "GET";
+            WebResponse response = request.GetResponse();
+            string answer = string.Empty;
+            using (Stream s = response.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(s))
+                {
+                    answer = reader.ReadToEnd();
+                }
+            }
+            response.Close();
+            richTextBox1.Text = answer;
+
+            // Вызываем копирование для данной GoPro
+            LoadFromGoPro(textBox1.Text);
+
+        }
+
+
+        public void LoadFromGoPro(string goproUri)
+        {
+            DownloadParameters input = new DownloadParameters(goproUri);
+            this.backgroundWorker1.RunWorkerAsync(input);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        // Проверка наличия события и привязка камеры
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Connecting = true;
+
+            
+
+            timer1.Stop();
+            Thread.Sleep(5000);
+            
+           
+                //     MessageBox.Show("GOPRO");
+                    foreach (var mo in new ManagementObjectSearcher(null, "SELECT * FROM Win32_PnPEntity").Get().OfType<ManagementObject>())
+                    {
+                        var argsd = new object[] { new string[] { "DEVPKEY_Device_FriendlyName", "DEVPKEY_Device_LastKnownParent" }, null };
+                        // or this works too using the PK's value formatted as string
+                        //var argsd = new object[] { new string[] { "DEVPKEY_Device_FriendlyName", "{83DA6326-97A6-4088-9453-A1923F573B29} 10" }, null };
+
+                        mo.InvokeMethod("GetDeviceProperties", argsd);
+                        var mbos = (ManagementBaseObject[])argsd[1]; // one mbo for each device property key
+                        var name = mbos[0].Properties.OfType<PropertyData>().FirstOrDefault().Value;
+                        if (name != null)
+                        {
+                            if (name.ToString().Contains(Global.Config.GoProNetworkPrefix))
+                            {
+                                var parent = mbos[1].Properties.OfType<PropertyData>().FirstOrDefault().Value;
+
+                                // для всех компонент            MessageBox.Show("NAME: " + name + " PARENT: " + parent);
+
+                                if (!serials.Contains(parent))
+                                {
+                                    serials.Add(parent.ToString());
+
+                                    MessageBox.Show("NAME: " + name + " PARENT: " + parent);
+                                }
+                            }
+
+                        }
+                    }
+                      
+                
+         //   MessageBox.Show("endtimer");
+            
+            
+            Connecting = false;
+        }
+        
+    }
+
+    static class Logger
+    {
+        // Запись последовательно без форматирования
+        public static void Write(string text)
+        {
+            using (StreamWriter sw = new StreamWriter(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\log.txt", true))
+            {
+                sw.Write(text);
+            }
+        }
+
+        // Запись строки с указанием времени
+        public static void WriteLine(string message)
+        {
+            using (StreamWriter sw = new StreamWriter(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\log.txt", true))
+            {
+                sw.WriteLine(String.Format("{0,-23} {1}", DateTime.Now.ToString() + ":", message));
+            }
+        }
+    }
+
+    static class Global
+    {
+        public static XMLConfig Config;
+    }
+
+    // Структура для хранения конфигурации программы
+    public struct XMLConfig
+    {
+        public string DestPath;                   // Папка назначения для копирования
+        public bool MakeErrorLogs;                // Ведение логов ошибок
+        public bool MakeWorkLogs;                 // Ведение логов копирования 
+        public bool DeleteAfterCopy;              // Удалять файлы после копирования
+        public string GoProNetworkPrefix;         // Часть наименования сетевого адаптера GoPro для поиска
+    }
+
+
+
+
+}
